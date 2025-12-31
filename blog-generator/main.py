5211a601d681c2bac81e1fd8a3ec49bd1ae1131a
Pacola <149804664+PacolaYes@users.noreply.github.com>
2025-12-31 14:04:18 -0300

    work some more on blog-generator

--- blog-generator/main.py
+++ blog-generator/main.py
@@ -4,6 +4,8 @@ import argparse
 from pathlib import Path
 from sys import exit
 
+from bs4 import BeautifulSoup, Comment
+
 def getMarkdown(path: str|None):
     if path is None:
         return
@@ -14,35 +16,60 @@ def getMarkdown(path: str|None):
         if entry.is_dir():
             files += getMarkdown(entry)
         elif entry.suffix == ".md":
-            print(entry)
+            # print(entry)
             files.append(entry)
     
     return files
 
-def convertMarkdown(files: list[Path]):
-    py_location = Path(__file__)
+def mdToHTML(files: list[Path], template: Path):
+    with open(template, "r", encoding="utf-8") as input_file:
+        template_str = input_file.read()
+    
+    # py_location = Path(__file__)
     for file in files:
-        name = file.stem
+        template_parser = BeautifulSoup(template_str, "html.parser")
+        # name = file.stem
 
-        markdown.markdownFromFile(input=str(file), output=str(py_location.parent.joinpath(Path("./output/" + name + ".html"))))
+        with open(file, "r", encoding="utf-8") as input_file:
+            text = input_file.read()
+        html = BeautifulSoup(markdown.markdown(text), "html.parser")
+        # html = markdown.markdown(input=str(file), output=str(py_location.parent.joinpath(Path("./output/" + name + ".html"))))
+        
+        found_comment = False
+        for comment in template_parser.find_all(string=lambda text: isinstance(text, Comment)):
+            if comment.strip() == "insert markdown":
+                found_comment = True
+                comment.replace_with(html)
+                print(comment)
+                break
+
+        if not found_comment:
+            break
+
+        print(template_parser.prettify())
 
 def main():
-    print("hey!")
+    # print("hey!")
     parser = argparse.ArgumentParser(
         prog="pac's blog build utlity",
         description="builds the blog pages from markdown",
         epilog="completely unnecessary, as i know there's tools that do this already"
     )
     parser.add_argument("input_directory", help="the folder it'll read the markdown files from")
-    arg = parser.parse_args().input_directory
+    parser.add_argument("output_directory", help="the folder where the blog entries will be in")
+    parser.add_argument("template_entry", help="the html file used as a template for the blog entries")
 
-    files = getMarkdown(arg)
+    args = parser.parse_args()
+    files = getMarkdown(args.input_directory)
+    template = Path(args.template_entry)
+    output = Path(args.output_directory)
 
-    if files is None:
-        return
+    if files is None \
+    or template.suffix != ".html" \
+    or not output.is_dir(): # yes, this isn't the best way to do html detection, i'd say
+        return "one of the arguments is invalid"
 
-    convertMarkdown(files)
+    mdToHTML(files, template)
 
 if __name__ == '__main__':
-    main()
-    exit()
+    exit(main())
