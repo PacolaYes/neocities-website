9a511d66b0683323a6452a95747dfc1c6b31ce6d
Pacola <149804664+PacolaYes@users.noreply.github.com>
2026-01-06 00:56:36 -0300

    blog-generator - work on it
    does generate the blog pages based on a markdown thing and template file, it seems.
    still gotta make it generate at the very least a list so it can show up on the blog stuff, maybe as a json object?

--- blog-generator/main.py
+++ blog-generator/main.py
@@ -1,10 +1,58 @@
 
-import markdown
+# vanilla python stuff
+# 1. idk
 import argparse
-from pathlib import Path
+import os
 from sys import exit
 
+# 2. stuff used in the markdown parser
+import re
+import datetime
+
+# 3. stuff used in the html stuff
+from pathlib import Path
+
+# markdown
+import markdown
+from markdown.extensions import Extension
+from markdown.preprocessors import Preprocessor
+
+# the html library
 from bs4 import BeautifulSoup, Comment
+from bs4.formatter import HTMLFormatter
+
+class processBlogStuff(Preprocessor):
+    def run(self, lines): # pretty much just the example modified :P
+        new_lines = []
+
+        # probably not the best code buuuut, it wooooorks :D
+        title_re = re.compile(r"(\[BlogTitle\] )(.*)")
+        date_re = re.compile(r"(\[BlogDateTime\] )(.*)")
+        for line in lines:
+            title = title_re.search(line)
+            date = date_re.search(line)
+            
+
+            if title and not self.md.title:
+                self.md.title = title[2]
+                continue
+
+            if date and not self.md.datetime:
+                self.md.datetime = datetime.datetime.strptime(date[2], "%d/%m/%Y %H:%M")
+                continue
+
+            new_lines.append(line)
+        return new_lines
+
+class blogExtension(Extension): # maybe i should just use the meta extension outright????? def would giv out more stuff 2 do
+    def extendMarkdown(self, md):
+        md.registerExtension(self)
+        self.md = md
+        md.preprocessors.register(processBlogStuff(md), 'blog-parse', 27)
+
+    def reset(self) -> None:
+        self.md.title = None
+        self.md.datetime = None
 
 def getMarkdown(path: str|None):
     if path is None:
@@ -16,9 +64,8 @@ def getMarkdown(path: str|None):
         if entry.is_dir():
             files += getMarkdown(entry)
         elif entry.suffix == ".md":
-            # print(entry)
+            print("Found entry: " + entry.name)
             files.append(entry)
-    
     return files
 
 def mdToHTML(files: list[Path], template: Path):
@@ -26,30 +73,38 @@ def mdToHTML(files: list[Path], template: Path):
         template_str = input_file.read()
     
     # py_location = Path(__file__)
+    md = markdown.Markdown(extensions=[blogExtension()], output_format="html")
+
+    html_files = []
+    formatter = HTMLFormatter(indent=4)
     for file in files:
         template_parser = BeautifulSoup(template_str, "html.parser")
-        # name = file.stem
 
         with open(file, "r", encoding="utf-8") as input_file:
             text = input_file.read()
-        html = BeautifulSoup(markdown.markdown(text), "html.parser")
-        # html = markdown.markdown(input=str(file), output=str(py_location.parent.joinpath(Path("./output/" + name + ".html"))))
+        html = BeautifulSoup(md.reset().convert(text), "html.parser")
+        if template_parser.title:
+            template_parser.title.string = md.title
         
         found_comment = False
         for comment in template_parser.find_all(string=lambda text: isinstance(text, Comment)):
             if comment.strip() == "insert markdown":
                 found_comment = True
                 comment.replace_with(html)
-                print(comment)
                 break
 
         if not found_comment:
             break
 
-        print(template_parser.prettify())
+        html_files.append({
+            "filename": Path(file.stem + ".html"),
+            "html": template_parser.prettify(formatter=formatter) # not the most ideal (for me), but it's better than no indentation at all!
+        })
+        print("Parsed entry: " + file.name)
+
+    return html_files
 
 def main():
-    # print("hey!")
     parser = argparse.ArgumentParser(
         prog="pac's blog build utlity",
         description="builds the blog pages from markdown",
@@ -65,11 +120,20 @@ def main():
     output = Path(args.output_directory)
 
     if files is None \
-    or template.suffix != ".html" \
-    or not output.is_dir(): # yes, this isn't the best way to do html detection, i'd say
+    or template.suffix != ".html": # yes, this isn't the best way to do html detection, i'd say
         return "one of the arguments is invalid"
 
-    mdToHTML(files, template)
+    html_files = mdToHTML(files, template)
+
+    if len(html_files) <= 0:
+        return "no HTML files were made"
+
+    os.makedirs(output, exist_ok = True)
+    for file in html_files:
+        path = str(output.joinpath(file["filename"]))
+        with open(path, "w", encoding="utf-8") as output_file:
+            output_file.write(file["html"])
+        print("Entry written to '" + path + "'!")
 
 if __name__ == '__main__':
     exit(main())
